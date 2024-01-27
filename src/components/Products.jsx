969b270be809270929c9f590dfea54f62a8a48ae
Bimbimmulya <bimbimmulya24@gmail.com>
2024-01-27 16:38:32 +0700

    Add useRef

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Skeleton from "react-loading-skeleton";
 import { NavLink } from "react-router-dom";
 
@@ -7,25 +7,24 @@ const Products = () => {
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
 
-  let componentMounted = true;
+  const componentMounted = useRef(true); // Menggunakan useRef
 
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products");
-      if (componentMounted) {
+      if (componentMounted.current) { // Menggunakan componentMounted.current
         setData(await response.clone().json());
         setFilter(await response.json());
         setLoading(false);
-        // console.log(filter);
       }
-
-      return () => {
-        componentMounted = false;
-      };
     };
 
     getProducts();
+
+    return () => {
+      componentMounted.current = false; // Mengubah nilai current saat komponen dibongkar
+    };
   }, []);
 
   const Loading = () => {
@@ -55,7 +54,7 @@ const Products = () => {
   const ShowProducts = () => {
     return (
       <>
-        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+        <div className="buttons d-flex justify-content-center mb-5 pb-5" key={filterProduct.category}>
           <button
             className="btn btn-outline-dark me-2"
             onClick={() => {
